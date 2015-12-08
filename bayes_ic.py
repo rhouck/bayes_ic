5593a676933ddba90ef2de6a4a528910da28f71a
Ryan Houck <ryan@levelskies.com>
2015-12-07 17:41:37 -0800

    checks for missing data in signal and target before fitting model on each column, added time series model

--- bayes_ic.py
+++ bayes_ic.py
@@ -1,9 +1,3 @@
-import numpy as np
-import pandas as pd
-import pymc3 as pm
-from pymc3.distributions.timeseries import *
-from theano.tensor import repeat
-
 class BayesIC(object):
     
     def __init__(self, signal, target):
@@ -33,7 +27,8 @@ class BayesIC(object):
         for i in self.target.columns:
             print
             print i
-            stats, trace = self._fit_model(self.signal[i], self.target[i], samples)
+            ind = sorted(set(self.signal[i].dropna().index.values) & set(self.target[i].dropna().index.values))
+            stats, trace = self._fit_single_period_model(self.signal[i].ix[ind], self.target[i].ix[ind], samples)
             stats['Id'] = i 
             betas.append(stats)
         self.betas = pd.DataFrame(betas)
@@ -48,7 +43,7 @@ class BayesIC(object):
         return self.signal * self.betas.set_index('Id')['prob_sub_zero']
         
     
-    def _fit_model(self, signal, target, samples):
+    def _fit_single_period_model(self, signal, target, samples):
 
         with pm.Model() as model:
 
@@ -67,5 +62,38 @@ class BayesIC(object):
             start = pm.find_MAP() # Find starting value by optimization
             step = pm.NUTS(state=start) # Instantiate MCMC sampling algorithm
             trace = pm.sample(samples, step, progressbar=True)
-        
+        print trace.beta.shape
         return self.get_stats(trace.beta), trace
+    
+    def _fit_time_series_model(self, signal, target, samples):
+        
+        model_randomwalk = pm.Model()
+        with model_randomwalk:
+
+            sigma_alpha = pm.Exponential('sigma_alpha', 1. / .02, testval=.1)
+            sigma_beta = pm.Exponential('sigma_beta', 1. / .02, testval=.1)
+
+            alpha = GaussianRandomWalk('alpha', sigma_alpha ** -2, shape=len(tar))
+            beta = GaussianRandomWalk('beta', sigma_beta ** -2, shape=len(tar))
+
+            # Define regression
+            regression = alpha + beta * rev.values
+
+            # Assume prices are Normally distributed, the mean comes from the regression.
+            sd = pm.Uniform('sd', 0, 20)
+            likelihood = pm.Normal('y', 
+                                   mu=regression, 
+                                   sd=sd, 
+                                   observed=tar.values)
+        
+            # First optimize random walk
+            start = pm.find_MAP(vars=[alpha, beta], fmin=optimize.fmin_l_bfgs_b)
+            step = pm.NUTS(scaling=start)
+            trace = pm.sample(10, step, start)
+
+            # Sample
+            start2 = trace.point(-1)
+            step = pm.NUTS(scaling=start2)
+            trace_rw = pm.sample(samples, step, start=start)
+            
+            
